9b172998d2e9c52de6c08de7c798bbe468e92e7b
schmi954 <schmidtjonmlg@gmail.com>
2022-04-29 13:55:16 -0500

    Initial commit

---
+++ housewebsite/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class HousewebsiteConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'housewebsite'
---
+++ pollproject/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PollprojectConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'pollproject'
